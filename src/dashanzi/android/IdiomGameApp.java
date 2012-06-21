9a93ef0803bc72dbaa8c76a1f1eac333f351d900
niujin <niujin@gmail.com>
2012-06-21 19:58:15 +0800

    init

--- src/dashanzi/android/IdiomGameApp.java
+++ src/dashanzi/android/IdiomGameApp.java
@@ -2,10 +2,19 @@ package dashanzi.android;
 
 import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 import dashanzi.android.dto.IMessage;
 
 public class IdiomGameApp extends Application {
 	private Activity currentActivity;
+
+	private String serverIp = "127.0.0.1";
+
+	private int serverPort = 12345;
+
+	public void onCreate(Bundle savedInstanceState) {
+
+	}
 
 	// ------------- public method ----------------------------
 	/**
@@ -29,6 +38,22 @@ public class IdiomGameApp extends Application {
 
 	public void setCurrentActivity(Activity currentActivity) {
 		this.currentActivity = currentActivity;
+	}
+
+	public String getServerIp() {
+		return serverIp;
+	}
+
+	public void setServerIp(String serverIp) {
+		this.serverIp = serverIp;
+	}
+
+	public int getServerPort() {
+		return serverPort;
+	}
+
+	public void setServerPort(int serverPort) {
+		this.serverPort = serverPort;
 	}
 
 }
