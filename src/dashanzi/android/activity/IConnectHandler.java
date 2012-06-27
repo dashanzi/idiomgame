696dc9960dbe193518a59f5f396057dbfbc9db93
juzm <juzm@dsp.ac.cn>
2012-06-27 10:34:15 +0800

    EE

--- src/dashanzi/android/activity/IConnectHandler.java
+++ src/dashanzi/android/activity/IConnectHandler.java
@@ -2,7 +2,13 @@ package dashanzi.android.activity;
 
 public interface IConnectHandler {
 
+	/**
+	 * socket连接成功回调方法
+	 */
 	public void handle();
 	
-//	pubulic void 
+	/**
+	 * socket连接失败、异常回调方法
+	 */
+	public void exceptionCatch();
 }
