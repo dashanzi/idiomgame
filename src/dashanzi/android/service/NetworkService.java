318f4ec079c7eaf4eab5fae99641920417a3e18c
niujin <niujin@gmail.com>
2012-06-29 11:32:12 +0800

    aaa

--- src/dashanzi/android/service/NetworkService.java
+++ src/dashanzi/android/service/NetworkService.java
@@ -29,18 +29,6 @@ public class NetworkService extends Service {
 
 	public boolean readFlag = true;
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		// Log.i("NetworkService", "service onBind");
-		return binder;
-	}
-
-	public class MyBinder extends Binder {
-		public NetworkService getService() {
-			return NetworkService.this;
-		}
-	}
-
 	// ------------- public methods ----------------------------
 	public void connect(String ip, int port) {
 		try {
@@ -50,30 +38,14 @@ public class NetworkService extends Service {
 			socket.connect(new InetSocketAddress(ip, port), 10000);
 			is = new BufferedReader(new InputStreamReader(
 					socket.getInputStream()));
-			// os = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
-			// socket.getOutputStream())), true);
 			os = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
 					socket.getOutputStream())), true);
 
 			Log.i("==NET==", "connected");
 
-			// if (socket.isConnected()) {
-			// if (!socket.isOutputShutdown()) {
-			// // os.print("Are you sb?");
-			//
-			// socket.getOutputStream()
-			// .write(new String(
-			// "{\"header\":{\"type\":\"JOIN_REQ\"},\"body\":{\"name\":\"张三\",\"gid\":\"g01\"}}\r\n"
-			// .getBytes("UTF-8"), "UTF-8")
-			// .getBytes("UTF-8"));
-			//
-			// }
-			// }
-
 			// 2. start reader
 			new Thread(readerThread).start();
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			Log.i("==NET==", "IOException caught when connecting: " + e);
@@ -83,8 +55,6 @@ public class NetworkService extends Service {
 			intent.putExtra("code", "1");
 			intent.setAction("android.intent.action.test");
 			sendBroadcast(intent);
-			// TODO Auto-generated catch block
-			// e.printStackTrace();
 		}
 
 	}
@@ -99,12 +69,7 @@ public class NetworkService extends Service {
 		if (socket != null && socket.isConnected()) {// edited by juzm socket !=
 														// null TODO
 			if (!socket.isOutputShutdown()) {
-				// os.print("Are you sb?");
-
 				try {
-					// socket.getOutputStream()
-					// .write("{\"header\":{\"type\":\"JOIN_REQ\"},\"body\":{\"name\":\"zhangsan\",\"gid\":\"g01\"}}\r\n"
-					// .getBytes("utf-8"));
 					Log.i("==NET==", "write 1");
 					socket.getOutputStream().write(
 							Beans2JsonUtil.getJsonStr(msg).getBytes("utf-8"));
@@ -112,8 +77,6 @@ public class NetworkService extends Service {
 				} catch (UnsupportedEncodingException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
-					// Log.i("==NET==","socket.isConnected()="+socket.isConnected());
-					// Log.i("==NET==","socket.isOutputShutdown()="+socket.isOutputShutdown());
 
 					Intent intent = new Intent();
 					intent.putExtra("status", "error");
@@ -123,7 +86,6 @@ public class NetworkService extends Service {
 
 					e.printStackTrace();
 				}
-
 			}
 		}
 	}
@@ -147,7 +109,7 @@ public class NetworkService extends Service {
 	}
 
 	// ------------- private methods ----------------------------
-	public void onMessageRecevied(String strMsg) {
+	private void onMessageRecevied(String strMsg) {
 		Log.i("==NET==", "message received: " + strMsg);
 
 		Intent intent = new Intent();
@@ -188,48 +150,37 @@ public class NetworkService extends Service {
 
 	};
 
-	// ------------- other ----------------------------
+	// ------------- other methods ----------------------------
 
 	public void onCreate() {
 		Log.i("==NET==", "service created");
 		super.onCreate();
 	}
+	
+	public IBinder onBind(Intent intent) {
+		// Log.i("NetworkService", "service onBind");
+		return binder;
+	}
 
 	public void onStart(Intent intent, int startId) {
 		super.onStart(intent, startId);
-		// new Thread() {
-		//
-		// public void run() {
-		// int i = 0;
-		// while (!isStop) {
-		// Intent intent = new Intent();
-		// intent.putExtra("i", i);
-		// i++;
-		// intent.setAction("android.intent.action.test");
-		// sendBroadcast(intent);
-		// Log.i("ttt", "Broadcasting... " + String.valueOf(i));
-		// try {
-		// sleep(1000);
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
-		// }
-		// }
-		// }.start();
-
 	}
 
 	public int onStartCommand(Intent i, int a, int b) {
-		// Log.i("==NET==", "Services onStartCommand");
-
 		return 0;
 	}
 
-	@Override
 	public void onDestroy() {
 		Log.i("==NET==", "service destroyed");
 		isStop = true;
 		readFlag = false;
 		super.onDestroy();
+	}
+	
+	// ------------- inner classes ----------------------------
+	public class MyBinder extends Binder {
+		public NetworkService getService() {
+			return NetworkService.this;
+		}
 	}
 }
